6c6e3cf80c3a195b06937df4ef423a219011a3b4
Ivan-D10 <141879392+Ivan-D10@users.noreply.github.com>
2023-10-24 18:06:15 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Inicio</title>
+</head>
+<body>
+
+    <header>
+        <nav id="navbar">
+            <div class="container">
+                <ul class="textbutton">
+                    <li>
+                        <a href="#">Inicio</a>
+                    </li>
+                    <li>
+                        <a href="#">Acerca de</a>
+                    </li>
+                    <li>
+                        <a href="#">Menú</a>
+                    </li>
+                    <li>
+                        <a href="#">Contacto</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        </nav>
+        <img src="images/header.jpg" id="banner">
+        <div class="contentxt">
+            <div class="text1">
+                Los mejores <b>Sabores</b>
+            </div>
+            <div class="text2">
+                <b>ESPECIALMENTE PARA TÍ</b>
+            </div>
+        </div>
+    </header>
+
+    <section><br><br><br><br>
+        <div class="precios">
+            <div class="cub">
+                <div>
+                <img src="images/Cubiertos.png" width="50px" height="50px">
+                </div>
+                <div>
+                    <b>INDIVIDUAL</b>
+                </div>
+                <div>
+                    $60.00
+                </div>
+            </div>
+
+            <div class="cub">
+            <div>
+                <img src="images/Cubiertos.png" width="50px" height="50px">
+                </div>
+                <div>
+                    <b>MEDIANA</b>
+                </div>
+                <div>
+                    $85.00
+                </div>
+            </div>
+
+            <div class="cub">
+            <div>
+                <img src="images/Cubiertos.png" width="50px" height="50px">
+                </div>
+                <div>
+                    <b>FAMILIAR</b>
+                </div>
+                <div>
+                    $100.00
+                </div>
+            </div>
+        </div>
+    </section>
+    
+    <section><BR><BR></BR></BR>
+        <div class="section2"><br><br>
+            <div class="nuestras"><b>NUESTRAS ESPECIALIDADES</b></div><br><br>
+
+            <div class="imgs">
+                <div class="col">
+                    <img src="images/peperoni.jpg" class="img1">
+                </div>
+
+                <div class="col">
+                    <img src="images/hawaiiana.jpg" class="img1">
+                </div>
+
+                <div class="col">
+                    <img src="images/vegetariana.jpg" class="img1">
+                </div>
+            </div>
+            <br>
+        </div>
+    </section>
+
+    <footer><br>
+        <div class="foot1">
+            <div><b>ENCUENTRANOS</b></div><br>
+            <div>Tuxtla Gutierrez, Chiapas</div>
+        </div><br>
+    </footer>
+</body>
+</html>
